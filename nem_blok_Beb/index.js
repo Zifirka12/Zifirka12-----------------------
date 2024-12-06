9113f35d7a16e6bdf8ad1293ec19c4dc124ef4f8
Student <Ilaafanasef21@gmail.com>
2024-12-06 10:06:06 +0300

    дзз

---
+++ nem_blok_Beb/index.js
@@ -0,0 +1,28 @@
+alert('10');
+        alert('20');
+        alert('2007');
+        alert('Брендан Эйх или Айк');
+        
+    let a = 10;
+    let b = 2;
+    let с = 9;
+
+
+    let sum = a + b;
+    let difference = a - b;
+    let product = a * b;
+    let quotient = a / b;
+    let result = b**5;
+
+    alert("Сумма: " + sum);
+    alert("Разность: " + difference);
+    alert("Произведение: " + product);
+    alert("Частное: " + quotient);
+    alert("В степени: " + result);
+
+    let g = 2;
+    let v = 9;
+    let six = v % g;
+    alert("Остаток: " + six);
+
+    
